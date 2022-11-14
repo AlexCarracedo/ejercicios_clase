76f8435ff61795e81d732d21a4a0e19ac6108e14
AlexCarracedo <115996770+AlexCarracedo@users.noreply.github.com>
2022-11-14 14:46:52 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
+let i=0;
 //Corre solo una vez cuando se inicia el programa
 function setup(){
+    
     console.log("setup - frameCount:"+frameCount)
 
     //createCanvas(ancho, alto en píxeles)
@@ -10,13 +12,23 @@ function setup(){
     // GREEN --> background(0,255,0);
     // BLUE --> background(0,0,255);
     //background(color('green'));
-    background(0,255,0)
 }
 //Corre continuamente después de la función setup
+
 function draw(){
+//Instrucciones a ejecutar en bucle
+    clear();
+    background(0,255,0);
     fill(100,50,215)
     ellipse(200,200,100,80);
     fill(0,0,255);
     rect(300,300,100,50);
-//Instrucciones a ejecutar en bucle
+    
+    fill(255,0,0);
+    rect(0,i,100,50);
+    i = i + 1
+    if (i==400) {
+        i=0
+    }
+    
 }
